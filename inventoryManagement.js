c5dfc93f52977fef36b7e000613e5a5c2ffcb807
Roseline001 <roseline.jonathan@student.moringaschool.com>
2026-09-24 12:08:05 +0300

    js arrays compulsory lab

--- inventoryManagement.js
+++ inventoryManagement.js
@@ -1,6 +1,24 @@
 // Write your code here
+let products = ["Laptop", "Phone", "Headphones", "Monitor"]
+// creating a function to console log details of the first product
+function logFirstProduct() {
+  console.log(products[0]);
+}
 
+// function to add a new product
+function addProduct(newProduct) {
+  products.push(newProduct);
+}
 
+// function to change the name of a product
+function updateProductName(position, newName) {
+  products[position] = newName;
+}
+
+//function to remove the last product in the array
+function removeLastProduct(delLastProduct) {
+  products.pop();
+}
 
 // Export the necessary parts for testing
 module.exports = {
